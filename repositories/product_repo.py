cf1792cf6d7ad436a63de221787e5a4707610dc2
alejandrogurtsov <alex.1046296@gmail.com>
2026-04-26 18:45:41 +0800

    feat: get and decrease quantity

--- repositories/product_repo.py
+++ repositories/product_repo.py
@@ -1,5 +1,5 @@
 """
-    запрос всех продуктов по категориям и количеством на складе
+    запрос всех продуктов по категориям, количество на складе, списание позиций
 """
 
 from db.connection import get_connection
@@ -15,3 +15,21 @@ class ProductRepository:
         ).fetchall()
         conn.close()
         return [dict(row) for row in rows]
+
+    def get_quantity(self, id_product):
+        conn = get_connection()
+        row = conn.execute(
+            'SELECT quantity_at_storage FROM products WHERE id_product = ?',
+            (id_product,)
+        ).fetchone()
+        conn.close()
+        return row['quantity_at_storage'] if row else 0
+
+    def decrease_quantity(self, id_product, quantity):
+        conn = get_connection()
+        conn.execute(
+            'UPDATE products SET quantity_at_storage = quantity_at_storage - ? WHERE id_product = ?',
+            (quantity, id_product)
+        )
+        conn.commit()
+        conn.close()
